a37fcbb7953a652f05a4ecef99aa23c643b65513
ibrahim abdullaziz <ibrahimabdullaziz55@gmail.com>
2026-09-05 21:37:36 +0300

    feat: add unit reviews service with create and retrieve functionalities

---
+++ src/modules/unit-reviews/unit-reviews.service.ts
@@ -0,0 +1,50 @@
+import ApiError from "../../common/utils/ApiError";
+import prisma from "../../db/prisma";
+
+export async function createReview(
+  guestId: string,
+  unitId: string,
+  rating: number,
+  comment: string,
+) {
+  const booking = await prisma.booking.findFirst({
+    where: {
+      guestId: guestId,
+      unitId: unitId,
+      status: "COMPLETED",
+    },
+  });
+
+  if (!booking) {
+    throw new ApiError(403, "You can`t access this booking");
+  }
+
+  const review = await prisma.unitReview.create({
+    data: {
+      unitId,
+      guestId,
+      rating,
+      comment,
+    },
+  });
+
+  if (!review) {
+    throw new ApiError(500, "Server Error while creation process");
+  }
+
+  return review;
+}
+
+export async function getUnitReviews(unitId: string) {
+  const reviews = await prisma.unitReview.findMany({
+    where: { unitId: unitId },
+    include: { unit: true },
+  });
+
+  const avgRating = await prisma.unitReview.aggregate({
+    _avg: { rating: true },
+    where: { unitId: unitId },
+  });
+
+  return { reviews, avgRating };
+}
